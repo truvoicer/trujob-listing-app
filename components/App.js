723ab4d953abc8b95a1a32274bbfafcb12738e2a
Michael Toriola <mikydxl@gmail.com>
2025-03-22 18:20:01 +0000

    Connect App component to Redux store for page and settings state management

--- components/App.js
+++ components/App.js
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react';
 import {ViewFactory} from "@/library/view/ViewFactory";
 import { setPageAction } from '@/library/redux/actions/page-actions';
 import { setSettingsAction } from '@/library/redux/actions/settings-actions';
+import { connect } from 'react-redux';
+import { PAGE_STATE } from '@/library/redux/constants/page-constants';
+import { SETTINGS_STATE } from '@/library/redux/constants/settings-constants';
 
-function App({data, settings}) {
+function App({data, settings, pageState, settingsState}) {
     const viewFactory = new ViewFactory();
     const view = viewFactory.renderView(data, settings);
     console.log(data, settings)
@@ -15,12 +18,17 @@ function App({data, settings}) {
     useEffect(() => {
         setSettingsAction(settings);
     }, [settings]);
-    
+    console.log({pageState, settingsState})
     return (
         <div>
-            {view? view : 'No view found'}
+            {/* {view? view : 'No view found'} */}
         </div>
     );
 }
 
-export default App;
+export default connect(
+    state => ({
+        pageState: state[PAGE_STATE],
+        settingsState: state[SETTINGS_STATE]
+    })
+)(App);
