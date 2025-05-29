dab5d9e9ed0efcc24583564ace15980a63649975
Michael Toriola <mikydxl@gmail.com>
2025-05-29 22:55:41 +0100

    refactor: Simplify page loading logic and remove unused constants in App component

--- components/App.tsx
+++ components/App.tsx
@@ -1,15 +1,15 @@
 'use client';
 import React, { useEffect } from 'react';
-import { ViewFactory } from "@/library/view/ViewFactory";
 import { setPageAction } from '@/library/redux/actions/page-actions';
 import { setSettingsAction } from '@/library/redux/actions/settings-actions';
 import { connect } from 'react-redux';
-import { PAGE_LOADED, PAGE_STATE } from '@/library/redux/constants/page-constants';
+import { PAGE_STATE } from '@/library/redux/constants/page-constants';
 import { setSiteAction } from '@/library/redux/actions/site-actions';
 import Loader from './Loader';
 import { setAppModeAction } from '@/library/redux/actions/app-actions';
 import ViewLayout from './Layout/ViewLayout';
 import { isObject, isObjectEmpty } from '@/helpers/utils';
+import { usePathname } from 'next/navigation';
 
 type Props = {
     data: any;
@@ -18,14 +18,26 @@ type Props = {
     page: any;
 }
 function App({ data, settings, site, page }: Props) {
+
+    const pathname = usePathname();
+
     function getAppMode() {
         const dark = localStorage.getItem('dark');
         return dark === 'true' ? 'dark' : 'light';
     }
 
+    function isPageLoaded() {
+        if (!isObject(page) || isObjectEmpty(page)) {
+            return false;
+        }
+        if (page?.permalink === pathname) {
+            return true;
+        }
+        return false;
+    }
+
     useEffect(() => {
-        let pageData = {...data};
-        pageData[PAGE_LOADED] = true;
+        let pageData = { ...data };
         setPageAction(pageData);
     }, [data]);
 
@@ -40,13 +52,12 @@ function App({ data, settings, site, page }: Props) {
     useEffect(() => {
         setAppModeAction(getAppMode());
     }, []);
-
-
+    
     return (
         <div>
-            {(page[PAGE_LOADED])
+            {isPageLoaded()
                 ? <ViewLayout />
-                
+
                 : <Loader fullScreen />
             }
         </div>
